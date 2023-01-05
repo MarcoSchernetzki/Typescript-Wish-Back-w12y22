0dd338989ac3743a0862bd8a1537ede9358d00a5
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-05 13:38:06 +0100

    Fixing file dbConnect

--- src/db/db.connect.ts
+++ src/db/db.connect.ts
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
-import { USER1, CLUSTER, PASSW } from '../config.js';
+import { USER, CLUSTER, PASSW } from '../config.js';
 
 export async function dbConnect() {
     const DBName = process.env.NODE_ENV !== 'test' ? 'Wishes' : 'WishesTesting';
-    let uri = `mongodb+srv://${USER1}:${PASSW}`;
+    let uri = `mongodb+srv://${USER}:${PASSW}`;
     uri += `@${CLUSTER}/${DBName}?retryWrites=true&w=majority`;
     return mongoose.connect(uri);
 }
