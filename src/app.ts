5ce857278380c67b564fbfcc869c7f194506ac16
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-05 13:14:15 +0100

    add initial config

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import { setCors } from './middlewares/cors.js';
+import { errorManager } from './middlewares/errors.js';
+import { wishesRouter } from './router/wishes.router.js';
+import { usersRouter } from './router/users.router.js';
+
+export const app = express();
+app.disable('x-powered-by');
+
+const corsOptions = {
+    origin: '*',
+};
+
+app.use(morgan('dev'));
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use(setCors);
+app.use(errorManager);
+
+app.get('/', (_req, res) => {
+    res.send('iWish App').end();
+});
+
+app.use('/wishes', wishesRouter);
+app.use('/users', usersRouter);
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const USER1 = process.env.USER1;
+export const PASSW = process.env.PASSW;
+export const CLUSTER = process.env.CLUSTER;
+export const SECRET = process.env.SECRET;
