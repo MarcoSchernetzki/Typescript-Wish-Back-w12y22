d27c8710be6e1eb3ee1893ea222d5cb40a9821fe
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-05 14:32:06 +0100

    Add routes user and wish in app

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import morgan from 'morgan';
 import { setCors } from './middlewares/cors.js';
 import { errorManager } from './middlewares/errors.js';
+import { wishesRouter } from './router/wishes.router.js';
+import { usersRouter } from './router/users.router.js';
 
 export const app = express();
 app.disable('x-powered-by');
@@ -22,5 +24,5 @@ app.get('/', (_req, res) => {
     res.send('iWish App').end();
 });
 
-//app.use('/wishes', wishesRouter);
-//app.use('/users', usersRouter);
+app.use('/wishes', wishesRouter);
+app.use('/users', usersRouter);
